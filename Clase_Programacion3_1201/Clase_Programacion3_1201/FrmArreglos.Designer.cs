1e63068c310fe60a99467edd993efb378a1d340c
DavidMatute <86425726+DavidMatute@users.noreply.github.com>
2021-06-29 16:39:19 -0600

    primer commit
    Este es el primer commit de la clase de programacion 3 de la seccion 1201

---
+++ Clase_Programacion3_1201/Clase_Programacion3_1201/FrmArreglos.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Clase_Programacion3_1201
+{
+    partial class FrmArreglos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_Arreglo = new System.Windows.Forms.ListBox();
+            this.btn_Ejecutar = new System.Windows.Forms.Button();
+            this.listBox_Paises = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox_Arreglo
+            // 
+            this.listBox_Arreglo.FormattingEnabled = true;
+            this.listBox_Arreglo.Location = new System.Drawing.Point(36, 200);
+            this.listBox_Arreglo.Name = "listBox_Arreglo";
+            this.listBox_Arreglo.Size = new System.Drawing.Size(106, 147);
+            this.listBox_Arreglo.TabIndex = 0;
+            // 
+            // btn_Ejecutar
+            // 
+            this.btn_Ejecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ejecutar.Location = new System.Drawing.Point(112, 61);
+            this.btn_Ejecutar.Name = "btn_Ejecutar";
+            this.btn_Ejecutar.Size = new System.Drawing.Size(116, 64);
+            this.btn_Ejecutar.TabIndex = 1;
+            this.btn_Ejecutar.Text = "Ejecutar";
+            this.btn_Ejecutar.UseVisualStyleBackColor = true;
+            this.btn_Ejecutar.Click += new System.EventHandler(this.btn_Ejecutar_Click);
+            // 
+            // listBox_Paises
+            // 
+            this.listBox_Paises.FormattingEnabled = true;
+            this.listBox_Paises.Location = new System.Drawing.Point(158, 200);
+            this.listBox_Paises.Name = "listBox_Paises";
+            this.listBox_Paises.Size = new System.Drawing.Size(160, 147);
+            this.listBox_Paises.TabIndex = 2;
+            // 
+            // FrmArreglos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox_Paises);
+            this.Controls.Add(this.btn_Ejecutar);
+            this.Controls.Add(this.listBox_Arreglo);
+            this.Name = "FrmArreglos";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_Arreglo;
+        private System.Windows.Forms.Button btn_Ejecutar;
+        private System.Windows.Forms.ListBox listBox_Paises;
+    }
+}
+
---
+++ Clase_Programacion3_1201/Clase_Programacion3_1201/FrmArreglos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_Programacion3_1201
+{
+    public partial class FrmArreglos : Form
+    {
+        public FrmArreglos()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Ejecutar_Click(object sender, EventArgs e)
+        {
+            //Formas de decalrar arreglos tipo INT
+            //int[] edades;
+            int[] edades = new int[5];
+
+            //Formas de llenar un arreglo
+            edades[0] = 10;
+            edades[1] = 20;
+            edades[2] = 30;
+            edades[3] = 40;
+            edades[4] = 50;
+
+            //Mostrar el arreglo
+            for (int i = 0; i < edades.Length; i++)
+            {
+                listBox_Arreglo.Items.Add(edades[i]);
+            }
+
+            //Formas de decalrar arreglos tipo STRING
+            string[] paises = new string[5] { "Honduras", "Nicaragua", "Costa Rica", "Guatemala", "El Salvador" };
+
+            //for (int i = 0; i < paises.Length; i++)
+            //{
+            //    listBox_Paises.Items.Add((i+1)+")." + " " + paises[i]);
+            //}
+
+            foreach (var item in paises)
+            {
+                listBox_Paises.Items.Add(item);
+            }
+
+        }
+    }
+}
